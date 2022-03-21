05ab403ba6c3917867717ef5fe1456bc9166b175
phaheez <faizfasasi@gmail.com>
2022-03-22 00:31:10 +0100

    Dependency Inversion Principle Implementation

--- DIP/Program.cs
+++ DIP/Program.cs
@@ -6,7 +6,12 @@ namespace DIP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var empManager = new EmployeeManager();
+            empManager.AddEmployee(new Employee { Name = "Leen", Gender = Gender.Female, Position = Position.Manager });
+            empManager.AddEmployee(new Employee { Name = "Mike", Gender = Gender.Male, Position = Position.Administrator });
+
+            var stats = new EmployeeStatistics(empManager);
+            Console.WriteLine($"Number of female managers in our company is: {stats.CountFemaleManagers()}");
         }
     }
 }
