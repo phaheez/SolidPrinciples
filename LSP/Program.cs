92837a7e25a1af50b7e92da0afb803d293aa744a
phaheez <faizfasasi@gmail.com>
2022-03-21 23:46:00 +0100

    Liskov Substitution Principle Implemetation

--- LSP/Program.cs
+++ LSP/Program.cs
@@ -6,7 +6,15 @@ namespace LSP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new int[] { 5, 7, 9, 8, 1, 6, 4 };
+
+            Calculator sum = new SumCalculator(numbers);
+            Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}");
+
+            Console.WriteLine();
+
+            Calculator evenSum = new EvenNumbersSumCalculator(numbers);
+            Console.WriteLine($"The sum of all the even numbers: {evenSum.Calculate()}");
         }
     }
 }
---
+++ LSP/SumCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    public class SumCalculator : Calculator
+    {
+        public SumCalculator(int[] numbers) : base(numbers) { }
+
+        public override int Calculate() => _numbers.Sum();
+    }
+}
