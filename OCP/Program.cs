43d5c0b7d38c5986c98df1bcea2907d00d70943b
phaheez <faizfasasi@gmail.com>
2022-03-21 23:24:36 +0100

    open-closed principle implementation

--- OCP/Program.cs
+++ OCP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OCP
 {
@@ -6,7 +7,15 @@ namespace OCP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var devCalculations = new List<BaseSalaryCalculator>
+            {
+                new SeniorDevSalaryCalculator(new DeveloperReport {Id = 1, Name = "Dev1", Level = "Senior developer", HourlyRate = 30.5, WorkingHours = 160 }),
+                new JuniorDevSalaryCalculator(new DeveloperReport {Id = 2, Name = "Dev2", Level = "Junior developer", HourlyRate = 20, WorkingHours = 150 }),
+                new SeniorDevSalaryCalculator(new DeveloperReport {Id = 3, Name = "Dev3", Level = "Senior developer", HourlyRate = 30.5, WorkingHours = 180 })
+            };
+
+            var calculator = new SalaryCalculator(devCalculations);
+            Console.WriteLine($"Sum of all the developer salaries is {calculator.CalculateTotalSalaries()} dollars");
         }
     }
 }
---
+++ OCP/SeniorDevSalaryCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    public class SeniorDevSalaryCalculator : BaseSalaryCalculator
+    {
+        public SeniorDevSalaryCalculator(DeveloperReport developerReport) : base(developerReport) { }
+
+        public override double CalculateSalary() => DeveloperReport.HourlyRate * DeveloperReport.WorkingHours * 1.2;
+    }
+}
